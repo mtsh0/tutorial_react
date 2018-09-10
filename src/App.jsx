1510554ce097339fd3ed081e4f21ce2aa7cfbe22
wtsh0 <t.ueno0207@gmail.com>
2018-09-10 12:33:24 +0900

    introduce youtube-api-search at npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header'
+import SearchInYoutube from 'youtube-api-search';
+import Header from './components/Header/Header';
 
 
 
+const youtube_api_key = 'AIzaSyA9LcEu99UT8OzJ2PcmLaE9K0UXrt1VdIk';
+
+SearchInYoutube({key: youtube_api_key, term: '猫 きゅうり'}, (data)=>{
+  console.log(data);
+});
 
 class App extends Component {
   render() {
