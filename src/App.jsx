65fb7c52a8d99bb430a2cb0b9e3f01bc26c87490
wtsh0 <t.ueno0207@gmail.com>
2018-09-11 10:22:02 +0900

    learned LCM(Life Cycle Method)

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,49 @@ import Header from './components/Header/Header';
 
 const youtube_api_key = 'AIzaSyA9LcEu99UT8OzJ2PcmLaE9K0UXrt1VdIk';
 
-SearchInYoutube({key: youtube_api_key, term: '猫 きゅうり'}, (data)=>{
-  console.log(data);
-});
+// SearchInYoutube({key: youtube_api_key, term: '猫 きゅうり'}, (data)=>{
+//   console.log(data);
+// });
 
 class App extends Component {
+
+  // stateの初期化
+  state = { videos: [] }
+
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
+
+  // componentDidMountでAPIリクエストを行う
+  componentDidMount() {
+    SearchInYoutube({ key: youtube_api_key, term: '猫 きゅうり' }, (data) => {
+      this.setState({ videos: data });
+    });
+
+    console.log("componentDidMount");
+  }
+
+  // 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('更新前のstate:' + this.state.videos);
+    console.log('更新後のstate:' + nextState.videos);
+    return true;
+  }
+
+  componentWillUpdate() {
+    console.log("componentWillUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidupdate");
+  }
+
+
+
   render() {
+
+    console.log(this.state.videos);
+
     return (
       <div>
         <div className="react-icon">
